5c1743d88ade55ce73d01eacc9f22f467b92112b
muhammedmahmutoglu <muhammed.mahmutoglu@outlook.com>
2024-03-20 04:39:21 +0300

    add sample card

---
+++ src/components/foodCard/FoodCard.jsx
@@ -0,0 +1,110 @@
+import "./foodCard.scss";
+import foodImage from "../../assets/img/cat-01.jpg";
+
+const FoodCard = () => {
+  return (
+    <div className="food-card">
+      <div className="food-card--content">
+        <div className="food-card--content__picture">
+          <img
+            // className="food-card--content__picture"
+            src={foodImage}
+            alt="food image"
+          />
+        </div>
+        <h5 className="food-card--content__type">Appetizer</h5>
+        <h4 className="food-card--content__name">
+          Crispy Chipotle Chicken Tacos
+        </h4>
+      </div>
+
+      <div className="food-card--content">
+        <div className="food-card--content__picture">
+          <img
+            // className="food-card--content__picture"
+            src={foodImage}
+            alt="food image"
+          />
+        </div>
+        <h5 className="food-card--content__type">Appetizer</h5>
+        <h4 className="food-card--content__name">
+          Crispy Chipotle Chicken Tacos
+        </h4>
+      </div>
+
+      <div className="food-card--content">
+        <div className="food-card--content__picture">
+          <img
+            // className="food-card--content__picture"
+            src={foodImage}
+            alt="food image"
+          />
+        </div>
+        <h5 className="food-card--content__type">Appetizer</h5>
+        <h4 className="food-card--content__name">
+          Crispy Chipotle Chicken Tacos
+        </h4>
+      </div>
+
+      <div className="food-card--content">
+        <div className="food-card--content__picture">
+          <img
+            // className="food-card--content__picture"
+            src={foodImage}
+            alt="food image"
+          />
+        </div>
+        <h5 className="food-card--content__type">Appetizer</h5>
+        <h4 className="food-card--content__name">
+          Crispy Chipotle Chicken Tacos
+        </h4>
+      </div>
+
+      <div className="food-card--content">
+        <div className="food-card--content__picture">
+          <img
+            // className="food-card--content__picture"
+            src={foodImage}
+            alt="food image"
+          />
+        </div>
+        <h5 className="food-card--content__type">Appetizer</h5>
+        <h4 className="food-card--content__name">
+          Crispy Chipotle Chicken Tacos
+        </h4>
+      </div>
+
+      <div className="food-card--content">
+        <div className="food-card--content__picture">
+          <img
+            // className="food-card--content__picture"
+            src={foodImage}
+            alt="food image"
+          />
+        </div>
+        <h5 className="food-card--content__type">Appetizer</h5>
+        <h4 className="food-card--content__name">
+          Crispy Chipotle Chicken Tacos
+        </h4>
+      </div>
+
+      <div className="food-card--content">
+        <div className="food-card--content__picture">
+          <img
+            // className="food-card--content__picture"
+            src={foodImage}
+            alt="food image"
+          />
+        </div>
+        <h5 className="food-card--content__type">Appetizer</h5>
+        <h4 className="food-card--content__name">
+          Crispy Chipotle Chicken Tacos
+        </h4>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default FoodCard;
