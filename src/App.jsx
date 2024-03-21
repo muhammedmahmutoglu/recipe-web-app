dff0b099b1380e93511f77f6e3fe62cedf431c05
muhammedmahmutoglu <muhammed.mahmutoglu@outlook.com>
2024-03-21 12:57:09 +0300

    add a roter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home"
+import Detail from "./pages/detail/Detail";
 import "./style/index.scss";
 
 const App = () => {
   return (
     <div>
-      <Home />
+       {
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail-page" element={<Detail />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      }
     </div>
   )
 }
