58de32a1ff570d51024a46ee549cea23fc053dcd
muhammedmahmutoglu <muhammed.mahmutoglu@outlook.com>
2024-03-21 06:19:06 +0300

    create a detail page

---
+++ src/pages/detail/Detail.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/footer/Footer"
+import Header from "../../components/header/Header"
+
+const Detail = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default Detail
