4402c1e8187fe2504122311e65a309c43c167979
muhammedmahmutoglu <muhammed.mahmutoglu@outlook.com>
2024-03-21 06:17:36 +0300

    use flex and props

--- src/components/foodCard/FoodCard.jsx
+++ src/components/foodCard/FoodCard.jsx
@@ -1,110 +1,26 @@
 import "./foodCard.scss";
-import foodImage from "../../assets/img/cat-01.jpg";
 
-const FoodCard = () => {
+import PropTypes from "prop-types";
+const FoodCard = ({ foodType, foodName, foodImage, foodLink }) => {
   return (
     <div className="food-card">
-      <div className="food-card--content">
+      <a href={foodLink} className="food-card--content ">
         <div className="food-card--content__picture">
-          <img
-            // className="food-card--content__picture"
-            src={foodImage}
-            alt="food image"
-          />
+          <img src={foodImage} alt="food image" />
+          <div className="shadow"></div>
         </div>
-        <h5 className="food-card--content__type">Appetizer</h5>
-        <h4 className="food-card--content__name">
-          Crispy Chipotle Chicken Tacos
-        </h4>
-      </div>
-
-      <div className="food-card--content">
-        <div className="food-card--content__picture">
-          <img
-            // className="food-card--content__picture"
-            src={foodImage}
-            alt="food image"
-          />
-        </div>
-        <h5 className="food-card--content__type">Appetizer</h5>
-        <h4 className="food-card--content__name">
-          Crispy Chipotle Chicken Tacos
-        </h4>
-      </div>
-
-      <div className="food-card--content">
-        <div className="food-card--content__picture">
-          <img
-            // className="food-card--content__picture"
-            src={foodImage}
-            alt="food image"
-          />
-        </div>
-        <h5 className="food-card--content__type">Appetizer</h5>
-        <h4 className="food-card--content__name">
-          Crispy Chipotle Chicken Tacos
-        </h4>
-      </div>
-
-      <div className="food-card--content">
-        <div className="food-card--content__picture">
-          <img
-            // className="food-card--content__picture"
-            src={foodImage}
-            alt="food image"
-          />
-        </div>
-        <h5 className="food-card--content__type">Appetizer</h5>
-        <h4 className="food-card--content__name">
-          Crispy Chipotle Chicken Tacos
-        </h4>
-      </div>
-
-      <div className="food-card--content">
-        <div className="food-card--content__picture">
-          <img
-            // className="food-card--content__picture"
-            src={foodImage}
-            alt="food image"
-          />
-        </div>
-        <h5 className="food-card--content__type">Appetizer</h5>
-        <h4 className="food-card--content__name">
-          Crispy Chipotle Chicken Tacos
-        </h4>
-      </div>
-
-      <div className="food-card--content">
-        <div className="food-card--content__picture">
-          <img
-            // className="food-card--content__picture"
-            src={foodImage}
-            alt="food image"
-          />
-        </div>
-        <h5 className="food-card--content__type">Appetizer</h5>
-        <h4 className="food-card--content__name">
-          Crispy Chipotle Chicken Tacos
-        </h4>
-      </div>
-
-      <div className="food-card--content">
-        <div className="food-card--content__picture">
-          <img
-            // className="food-card--content__picture"
-            src={foodImage}
-            alt="food image"
-          />
-        </div>
-        <h5 className="food-card--content__type">Appetizer</h5>
-        <h4 className="food-card--content__name">
-          Crispy Chipotle Chicken Tacos
-        </h4>
-      </div>
-
-      
+        {<h5 className="food-card--content__type">{foodType}</h5>}
+        <h4 className="food-card--content__name">{foodName}</h4>
+      </a>
     </div>
   );
+};
+
+FoodCard.propTypes = {
+  foodType: PropTypes.string.isRequired,
+  foodName: PropTypes.string.isRequired,
+  foodImage: PropTypes.string.isRequired,
+  foodLink: PropTypes.string.isRequired,
 };
 
 export default FoodCard;
