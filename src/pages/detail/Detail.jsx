29c4ac2f8782d7378464eaa5cb8c8c3d97d76021
muhammedmahmutoglu <muhammed.mahmutoglu@outlook.com>
2024-03-25 12:59:38 +0300

    make it responsive compatible

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,13 +1,33 @@
-import Footer from "../../components/footer/Footer"
-import Header from "../../components/header/Header"
+import RecipeDetail from "../../components/detailCard/RecipeDetail";
+import FlavorCard from "../../components/flavorCard/FlavorCard";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Ingredients from "../../components/ingredients/Ingredients";
+
+import "./detail.scss";
 
 const Detail = () => {
   return (
     <div className="detail">
-      <Header />
+      <Header headerLink={"/"} />
+      <div className="detail--content">
+        <FlavorCard
+          flavorTitle={"Pasta with tomato"}
+          flavorDescription={
+            "A light and refreshing pasta dish featuring fresh tomatoes, mozzarella, cheese, and fragrant basil, tossed with pasta for a simple and delicious meal that celebrates the flavors of summer."
+          }
+          flavorImage={
+            "https://assets.website-files.com/6455d3a136a229776428bcfb/6476ebf47bdc09608da5b136_big-re-02.jpg"
+          }
+        />
+        <div>
+          <RecipeDetail category={"Lunch"} cookTime={"30 Minutes"} nutrition={"221 kcal"} servings={"2-3"}  />
+          <Ingredients   />
+        </div>
+      </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
