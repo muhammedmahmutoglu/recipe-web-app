8ea4b289de8f9886c1b072c7f65b873dfe57bacd
muhammedmahmutoglu <muhammed.mahmutoglu@outlook.com>
2024-05-01 17:00:42 +0300

    add the author part

---
+++ src/components/author/Author.jsx
@@ -0,0 +1,27 @@
+import "./author.scss";
+const Author = () => {
+  return (
+    <div className="author">
+      <div className="author__image">
+        <img
+          src={
+            "https://ideacdn.net/shop/bd/29/myassets/products/754/siyah-asci-bandana-fab3781-vezera_min.jpg?revision=1697143329"
+          }
+          alt=""
+        />
+      </div>
+      <span className="author__name">Jahan ablodus</span>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ratione
+        perferendis fugiat. Fuga illum quasi odio, est sapiente libero harum
+        magnam tempora sed, consequuntur inventore, nesciunt officia vitae
+        dolorum. Facilis fugit sit nam nemo laboriosam voluptatibus ex soluta
+        iste dolorem, repudiandae possimus blanditiis. Ipsum quo at, adipisci,
+        asperiores nisi ipsa accusamus ut quia animi quos esse aut. Harum, a
+        dolores.
+      </p>
+    </div>
+  );
+};
+
+export default Author;
